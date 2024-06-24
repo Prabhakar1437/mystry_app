7a8bad14b66661bbf480ebc551742dc160603dd5
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-06-24 15:31:52 +0530

    bug fixed

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -181,7 +181,7 @@ function UserDashboard() {
         {messages.length > 0 ? (
           messages.map((message, index) => (
             <MessageCard
-              key={message._id}
+              key={message._id as string} // Fixed code
               message={message}
               onMessageDelete={handleDeleteMessage}
             />
